7ff8e057e9c75c90b528db81d27ecdd3899333f5
Vipul767 <62534176+Vipul767@users.noreply.github.com>
2020-11-13 20:19:50 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+
+setup(
+    name='TOPSIS-Vipul-101803491',
+    version='0.7',
+    author='Vipul Goel',
+    packages=['TOPSIS-Vipul-101803491'],
+    author_email='vgoel_be18@thapar.edu',
+    description='TOPSIS implementation',
+    
+    long_description_content_type="text/markdown",
+    url="https://github.com/Vipul767/TOPSIS-Vipul-101803491",
+    download_url = "https://github.com/Vipul767/TOPSIS-Vipul-101803491/archive/v_07.tar.gz",  
+    keywords=['TOSPSIS','Normalised_Matrix','Performance_score','Rank','Weighted_Normalised_matrix'],
+    install_requires=[
+                      'pandas'
+     ],
+	classifiers=[
+	'Development Status :: 3 - Alpha',     
+        'Intended Audience :: Developers',     
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 3',
+	'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.5',
+	'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
+    
+    
+)
